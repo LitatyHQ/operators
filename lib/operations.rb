1bd1894bee7f9a5863d5ca73899be80a6718e88d
LitatyHQ <litatyHQ@gmail.com>
2020-10-09 22:04:10 -0400

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,12 @@
-def unsafe(speed)
-    if speed > 40 || speed > 60
-        return "unsafe"
+def unsafe?(speed)
+    if speed > 60 || speed < 40
+        return true
     else
-        return "safe"
+        return false
+    end
 end
 
 
-def not_safe(speed)
-    speed > 40 || speed > 60 ? "unsafe" : "safe"
+def not_safe?(speed)
+    speed < 40 || speed > 60 ? true : false
 end
